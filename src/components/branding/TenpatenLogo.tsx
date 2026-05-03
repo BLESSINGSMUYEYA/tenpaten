dc0808960a1d6c8504482fee474463ea58bab1e9
Blessings <blessingsmuyeya@gmail.com>
2026-05-03 12:29:36 +0200

    feat: make TenpatenLogo a clickable link to home page

--- src/components/branding/TenpatenLogo.tsx
+++ src/components/branding/TenpatenLogo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 interface LogoProps extends React.SVGProps<SVGSVGElement> {
@@ -19,12 +20,12 @@ export function TenpatenIcon({ className, variant = 'color', ...props }: LogoPro
 
 export function TenpatenLogo({ className, variant = 'white', ...props }: LogoProps) {
     return (
-        <div className={cn("flex items-center", className)}>
+        <Link href="/" className={cn("flex items-center hover:opacity-90 transition-opacity", className)}>
             <img
                 src={variant === 'navy' ? "/tenpaten-logo-navy.png" : "/tenpaten-logo.png"}
                 alt="Tenpaten Logo"
                 className="h-10 w-auto object-contain"
             />
-        </div>
+        </Link>
     );
 }
